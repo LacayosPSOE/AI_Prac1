889d224c182bd7e9afcfb58445e410811984b3a7
VoltorH <darkkrasher@gmail.com>
2025-02-11 14:34:13 +0100

    No funciona pero lo he intentado jajaj

---
+++ Assets/Practica/Scripts/LEVIATHAN_Blackboard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LEVIATHAN_Blackboard : MonoBehaviour
+{
+    public float WanderMaxTime = 8f;
+    public float WanderTime = 0f;
+    public float speed = 8f;
+    public float timeToEatFish = 1.5f;
+    public float eatRadius = 1.5f;
+    public float eatMaxTimer = 1.5f;
+
+    public GameObject Home;
+
+
+
+}
