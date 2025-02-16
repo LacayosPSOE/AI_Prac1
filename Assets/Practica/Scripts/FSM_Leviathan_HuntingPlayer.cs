d564bdf0b3131157912c835867356ed7fe5264a7
VoltorH <130765229+VoltorH@users.noreply.github.com>
2025-02-16 15:50:12 +0100

    Player movement y otras cosas

--- Assets/Practica/Scripts/FSM_Leviathan_HuntingPlayer.cs
+++ Assets/Practica/Scripts/FSM_Leviathan_HuntingPlayer.cs
@@ -8,7 +8,7 @@ public class FSM_Leviathan_HuntingPlayer : FiniteStateMachine
     /* Declare here, as attributes, all the variables that need to be shared among
      * states and transitions and/or set in OnEnter or used in OnExit 
      * For instance: steering behaviours, blackboard, ...*/
-         private Seek seek;
+         private Arrive arrive;
     private LEVIATHAN_Blackboard blackboard;
     private SteeringContext steeringContext;
     private float stamina;
@@ -18,7 +18,7 @@ public class FSM_Leviathan_HuntingPlayer : FiniteStateMachine
         /* Write here the FSM initialization code. This code is execute every time the FSM is entered.
          * It's equivalent to the on enter action of any state 
          * Usually this code includes .GetComponent<...> invocations */
-        seek = GetComponent<Seek>();
+         arrive = GetComponent<Arrive>();
         blackboard = GetComponent<LEVIATHAN_Blackboard>();
         steeringContext = GetComponent<SteeringContext>();
         base.OnEnter(); // do not remove
@@ -56,18 +56,19 @@ public class FSM_Leviathan_HuntingPlayer : FiniteStateMachine
         );
 
         */
+        
         FiniteStateMachine EAT = ScriptableObject.CreateInstance<FSM_Leviathan_Eat>();
         EAT.name = "EAT";
          State chasePlayer = new State("Chase_Player",
-            () => { seek.target = blackboard.Player; seek.enabled = true; stamina = 0f; },
+            () => { arrive.target = blackboard.Player; arrive.enabled = true; stamina = 0f; },
             () => { stamina += Time.deltaTime;},
-            () => { seek.enabled = false; }
+            () => { arrive.enabled = false; }
         );
 
-        State eatState = new State("Eat_State",
+        State eatPlayer = new State("Eat_State",
             () => { blackboard.eatMaxTimer = 0f; },
             () => { blackboard.eatMaxTimer += Time.deltaTime;},
-            () => {blackboard.Player.SetActive(false); }
+            () => {Destroy(blackboard.Player); }
         );
   
 
@@ -83,15 +84,25 @@ public class FSM_Leviathan_HuntingPlayer : FiniteStateMachine
             () => { return SensingUtils.DistanceToTarget(gameObject, blackboard.Player) < blackboard.playerChaseRadius; },
             () => { }
         );
-
+               Transition playerWithinEatRange = new Transition("Player_Within_Eat_Range",
+            () => { return SensingUtils.DistanceToTarget(gameObject, blackboard.Player) < blackboard.eatRadius; },
+            () => { }
+        );
+                       Transition playerEaten = new Transition("Player_Within_Eat_Range",
+            () => { return blackboard.eatMaxTimer >= 1.5f; },
+            () => { }
+        );
         Transition playerFar = new Transition("Player_Far",
             () => { return SensingUtils.DistanceToTarget(gameObject, blackboard.Player) >= blackboard.playerChaseRadius || blackboard.maxStamina <= stamina; },
             () => { }
         );
 
-        AddStates(EAT, chasePlayer, eatState);
+        AddStates(EAT, chasePlayer, eatPlayer);
+        AddTransition(chasePlayer, playerWithinEatRange, eatPlayer);
         AddTransition(EAT, playerTooClose, chasePlayer);
         AddTransition(chasePlayer, playerFar, EAT);
+        AddTransition(eatPlayer, playerEaten, EAT);
+
 
         initialState = EAT;
 
