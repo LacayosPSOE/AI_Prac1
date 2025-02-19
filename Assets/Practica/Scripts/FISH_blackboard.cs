68cd051b19e91a6c3cb1e653ffe94be15ca43b07
joeljoanlopez <joeljoanlopez@gmail.com>
2025-02-19 14:41:55 +0100

    Make a fish group and FSM

---
+++ Assets/Practica/Scripts/FISH_blackboard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FISH_blackboard : MonoBehaviour
+{
+    [Header("Game Objects")]
+    public GameObject fishAttractor;
+    public GameObject leviathan;
+    
+    [Header("Distances")]
+    public float dangerousDistanceFromMonster = 5.0f;
+    public float safeDistanceFromMonster = 15.0f;
+    public float groupArrivedDistance = 2.0f;
+
+    [Header("Speeds")]
+    public float speed = 5.0f;
+    public float fleeSpeed = 10.0f;
+}
--- Assets/Practica/Scripts/FSM_FishPack.cs
+++ Assets/Practica/Scripts/FSM_FishPack.cs
@@ -1,71 +1,112 @@
 using FSMs;
 using UnityEngine;
 using Steerings;
+using Unity.VisualScripting;
+using State = FSMs.State;
 
 [CreateAssetMenu(fileName = "FSM_FishPack", menuName = "Finite State Machines/FSM_FishPack", order = 1)]
 public class FSM_FishPack : FiniteStateMachine
 {
-    /* Declare here, as attributes, all the variables that need to be shared among
-     * states and transitions and/or set in OnEnter or used in OnExit 
-     * For instance: steering behaviours, blackboard, ...*/
+	/* Declare here, as attributes, all the variables that need to be shared among
+	 * states and transitions and/or set in OnEnter or used in OnExit
+	 * For instance: steering behaviours, blackboard, ...*/
+	private FISH_blackboard blackboard;
+	private FleePlusOA flee;
+	private VelocityMatching velocityMatch;
+	private Arrive arrive;
+	private SteeringContext steeringContext;
 
-    public override void OnEnter()
-    {
-        /* Write here the FSM initialization code. This code is execute every time the FSM is entered.
-         * It's equivalent to the on enter action of any state 
-         * Usually this code includes .GetComponent<...> invocations */
-        base.OnEnter(); // do not remove
-    }
+	public override void OnEnter()
+	{
+		/* Write here the FSM initialization code. This code is execute every time the FSM is entered.
+		 * It's equivalent to the on enter action of any state
+		 * Usually this code includes .GetComponent<...> invocations */
+		blackboard = GetComponent<FISH_blackboard>();
+		flee = GetComponent<FleePlusOA>();
+		velocityMatch = GetComponent<VelocityMatching>();
+		arrive = GetComponent<Arrive>();
+		steeringContext = GetComponent<SteeringContext>();
+		base.OnEnter(); // do not remove
+	}
 
-    public override void OnExit()
-    {
-        /* Write here the FSM exiting code. This code is execute every time the FSM is exited.
-         * It's equivalent to the on exit action of any state 
-         * Usually this code turns off behaviours that shouldn't be on when one the FSM has
-         * been exited. */
-        base.OnExit();
-    }
+	public override void OnExit()
+	{
+		/* Write here the FSM exiting code. This code is execute every time the FSM is exited.
+		 * It's equivalent to the on exit action of any state
+		 * Usually this code turns off behaviours that shouldn't be on when one the FSM has
+		 * been exited. */
+		base.DisableAllSteerings();
+		base.OnExit();
+	}
 
-    public override void OnConstruction()
-    {
-        /* STAGE 1: create the states with their logic(s)
-         *-----------------------------------------------
-         
-        State varName = new State("StateName",
-            () => { }, // write on enter logic inside {}
-            () => { }, // write in state logic inside {}
-            () => { }  // write on exit logic inisde {}  
-        );
+	public override void OnConstruction()
+	{
+		State wanderInGroup = new State("Wander",
+			() =>
+			{
+				velocityMatch.target = blackboard.fishAttractor;
+				velocityMatch.enabled = true;
+			},
+			() => { },
+			() => { velocityMatch.enabled = false; }
+		);
 
-         */
+		State fleeFromMonster = new State("Flee",
+			() =>
+			{
+				steeringContext.maxSpeed = blackboard.fleeSpeed;
+				flee.target = blackboard.leviathan;
+				flee.enabled = true;
+			},
+			() => { },
+			() =>
+			{
+				steeringContext.maxSpeed = blackboard.speed;
+				flee.enabled = false;
+			}
+		);
 
+		State returnToGroup = new State("Return",
+			() =>
+			{
+				arrive.target = blackboard.fishAttractor;
+				arrive.enabled = true;
+			},
+			() => { },
+			() => { arrive.enabled = false; }
+		);
 
-        /* STAGE 2: create the transitions with their logic(s)
-         * ---------------------------------------------------
+		Transition monsterIsNear = new Transition("Monster Is Near",
+			() =>
+			{
+				return SensingUtils.DistanceToTarget(gameObject, blackboard.leviathan) <
+				       blackboard.dangerousDistanceFromMonster;
+			}
+		);
 
-        Transition varName = new Transition("TransitionName",
-            () => { }, // write the condition checkeing code in {}
-            () => { }  // write the on trigger code in {} if any. Remove line if no on trigger action needed
-        );
+		Transition monsterIsFar = new Transition("Monster Is Far",
+			() =>
+			{
+				return SensingUtils.DistanceToTarget(gameObject, blackboard.leviathan) >
+				       blackboard.safeDistanceFromMonster;
+			}
+		);
 
-        */
-
-
-        /* STAGE 3: add states and transitions to the FSM 
-         * ----------------------------------------------
-            
-        AddStates(...);
-
-        AddTransition(sourceState, transition, destinationState);
-
-         */ 
-
-
-        /* STAGE 4: set the initial state
-         
-        initialState = ... 
-
-         */
-
-    }
+		Transition arrivedToGroup = new Transition("Arrived To Group",
+			() =>
+			{
+				return SensingUtils.DistanceToTarget(gameObject, blackboard.fishAttractor) <
+				       blackboard.groupArrivedDistance;
+			}
+		);
+		
+		AddStates(wanderInGroup, fleeFromMonster, returnToGroup);
+		
+		AddTransition(wanderInGroup, monsterIsNear, fleeFromMonster);
+		AddTransition(returnToGroup, monsterIsNear, fleeFromMonster);
+		AddTransition(fleeFromMonster, monsterIsFar, returnToGroup);
+		AddTransition(returnToGroup, arrivedToGroup, wanderInGroup);
+		
+		initialState = wanderInGroup;
+	}
 }
